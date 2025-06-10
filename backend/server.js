0fed8bc16f16e41a084aa5125aad6792972b628e
Mujahid087 <mujahidmahedi10@gmail.com>
2025-06-10 12:54:39 +0530

    creating a website

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,16 @@ import express from "express"
 import cookieParser from "cookie-parser"
 import dotenv from "dotenv"
 import connectDB from "./config/db.js";
+import path from "path"
 
 
 
 // routes 
 
 import userRoutes from "./routes/userRoutes.js"
+import moviesRoutes from "./routes/moviesRoutes.js"
+import genreRoutes from "./routes/genreRoutes.js"
+import uploadRoutes from "./routes/uploadRoutes.js"
 
 const app=express();
 dotenv.config();
@@ -25,12 +29,14 @@ const PORT=process.env.PORT || 3000
 
 // routes 
 app.use("/api/v1/users",userRoutes)
+app.use("/ai/v1/movies",moviesRoutes)
+app.use("/api/v1/genre", genreRoutes);
+app.use("/api/v1/upload", uploadRoutes);
 
 
 
-
-
-
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
 
 
 
